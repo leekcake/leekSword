de658d6f5a7889d68b310c546bdb836f36bda905
Blue Electric <fkdlxmslrx@gmail.com>
2017-08-30 17:10:17 +0900

    Add SDSReader

---
+++ src/main/kotlin/moe/leekcake/sword/sds/SDSReader.kt
@@ -0,0 +1,13 @@
+package moe.leekcake.sword.sds
+
+import java.io.InputStream
+
+class SDSReader(val input: InputStream) {
+    fun close() {
+        input!!.close()
+    }
+
+    fun next(): SDS {
+        return SDS(input);
+    }
+}

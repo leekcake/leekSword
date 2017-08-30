90f3fcbd5e92f35c21bd230099e4d341319aa634
Blue Electric <fkdlxmslrx@gmail.com>
2017-08-30 17:05:33 +0900

    Add SDS(Simple Data Structure)

--- src/main/kotlin/moe/leekcake/sword/sds/SDS.kt
+++ src/main/kotlin/moe/leekcake/sword/sds/SDS.kt
@@ -1,7 +1,9 @@
 package moe.leekcake.sword.sds
 
-import java.io.InputStream
-import java.io.OutputStream
+import moe.leekcake.sword.Logger
+
+import moe.leekcake.sword.Logger.Companion.logger;
+import java.io.*
 
 /**
  * Simple(Stupid) Data Structure
@@ -11,11 +13,160 @@ import java.io.OutputStream
  * 이전 데이터 방식과의 호환성을 위해 사용
  */
 class SDS {
-    constructor(input: InputStream) {
+    val name: String;
+    val arguments: HashMap<String, Any?> = HashMap();
 
+    constructor(name: String, vararg arguments: Any?) {
+        this.name = name;
+        for(inx in 0 until arguments.size step 2) {
+            if(arguments[inx] !is String) {
+                throw Exception("Invalid argument, require like [string, any, string, any]");
+            }
+            this.arguments[ arguments[inx] as String ] = arguments[inx+1];
+        }
+    }
+
+    constructor(data: ByteArray) : this( ByteArrayInputStream(data) )
+
+    constructor(input: InputStream) {
+        val ois: ObjectInputStream;
+        if(input is ObjectInputStream) {
+            ois = input;
+        } else {
+            ois = ObjectInputStream(input)
+        }
+
+        name = SaveHelper.readString(ois);
+
+        val count: Int = ois.readInt();
+        for(inx in 0 until count) {
+            val nameOnly: Boolean = ois.readBoolean();
+            val name: String = SaveHelper.readString(ois);
+
+            if(nameOnly) {
+                putArgument(name, null);
+            } else {
+                putArgument(name, SaveHelper.readAuto(ois));
+            }
+        }
+    }
+
+    fun save(): ByteArray {
+        val baos = ByteArrayOutputStream();
+        save(baos);
+        return baos.toByteArray();
     }
 
     fun save(output: OutputStream) {
+        val oos: ObjectOutputStream;
+        if(output is ObjectOutputStream) {
+            oos = output;
+        } else {
+            oos = ObjectOutputStream(output);
+        }
 
+        SaveHelper.writeString(oos, name);
+
+        oos.writeInt( arguments.size );
+
+        for((key, value) in arguments) {
+            oos.writeBoolean( value === null );
+            SaveHelper.writeString(oos, key);
+            if(value !== null) {
+                SaveHelper.writeAuto(oos, value);
+            }
+        }
+
+        output.flush();
+    }
+
+    fun dump(level: Logger.Level) {
+        logger.print(level, "CMLS", toString());
+    }
+
+    fun dumpDetail(level: Logger.Level) {
+        logger.print(level, "CMLS", "Name: $name");
+        for((key, value) in arguments) {
+            if(value != null) {
+                logger.print(level, "CMLS", "argument '$key' = '$value'");
+            } else {
+                logger.print(level, "CMLS", "argument '$key' = nothing");
+            }
+        }
+    }
+
+    fun containsArgument(name: String): Boolean {
+        return arguments.containsKey(name);
+    }
+
+    fun getArgument(name: String): Any? {
+        return arguments[name];
+    }
+
+    fun getArgumentString(name: String): String? {
+        val argument: Any? = arguments[name];
+        if(argument === null) {
+            return null;
+        }
+
+        if( argument is String ) {
+            return argument;
+        } else {
+            return argument.toString();
+        }
+    }
+
+    fun getArgumentStringArray(name: String): Array<String>? {
+        val argument: Any? = arguments[name];
+
+        if(argument is Array<*>) {
+            return argument as Array<String>;
+        }
+
+        return null;
+    }
+
+    fun getArgumentNumber(name: String): Number {
+        val argument: Any? = arguments[name];
+
+        if( argument === null ) {
+            throw Exception("$name is Empty field");
+        }
+
+        if( argument is String ) {
+            return argument.toDouble();
+        }
+
+        if(argument !is Number) {
+            throw Exception("$name is not Number");
+        }
+
+        return argument;
+    }
+
+    fun putArgument(name: String, data: Any?) {
+        if( arguments.containsKey(name) ) {
+            arguments.remove(name);
+        }
+        arguments[name] = data;
+    }
+
+    override fun toString(): String {
+        val builder = StringBuilder();
+        builder.append(name);
+        builder.append(" {");
+        for((key, value) in arguments) {
+            builder.append(key);
+            if(value != null) {
+                builder.append("=");
+                builder.append( value.toString() );
+            }
+            builder.append(',');
+        }
+        //Cut last ','
+        builder.setLength( builder.length - 1 );
+        builder.append('}');
+
+        return builder.toString();
     }
 }
